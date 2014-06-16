1606ab5634342d38f6806016dbcd04767744c813
m1kc (Max Musatov) <m1kc@yandex.ru>
2014-06-16 13:22:59 +0400

    Tests.

---
+++ run-tests.sh
@@ -0,0 +1,44 @@
+number=0
+
+run_test(){
+	let number=${number}+1
+	echo "--- Test #${number}. This test $2."
+	echo "--- Compiling..."
+	echo "$1" | ./llvm-brainfuck | llvm-as | opt -instcombine | llc >test.s
+	echo "--- Assembly:"
+	#cat test.s
+	echo "--- Running..."
+	clang test.s shim.a -o test
+	./test
+	echo -e "\n--- Done, return code $?.\n"
+	rm -f test test.s
+}
+
+
+run_test \
+	"+++" \
+	"just checks if the compiler works"
+
+run_test \
+	",.+.+." \
+	"takes a letter, then prints it back, then prints two following ones"
+
+run_test \
+	">>>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++." \
+	"prints 'H'"
+
+run_test \
+	",.,.,.,.,." \
+	"five letters back"
+
+run_test \
+	"+++++[>,.<-]" \
+	"five letters back too, but using cycles"
+
+run_test \
+	"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++." \
+	"prints 'hello, world'"
+
+run_test \
+	"-,+[-[>>++++[>++++++++<-]<+<-[>+>+>-[>>>]<[[>+<-]>>+>]<<<<<-]]>>>[-]+>--[-[<->+++[-]]]<[++++++++++++<[>-[>+>>]>[+[<+>-]>+>>]<<<<<-]>>[<+>-]>[-[-<<[-]>>]<<[<<->>-]>>]<<[<<+>>-]]<[-]<.[-]<-,+]" \
+	"performs ROT13 transformation"
